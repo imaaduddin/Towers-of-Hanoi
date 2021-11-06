4ac72446360a0c936f10b50ad02465d8614c95ee
Imaad Uddin <imaaduddin27@gmail.com>
2021-11-06 01:34:28 -0500

    my work so far

---
+++ script.py
@@ -0,0 +1,34 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+stacks += [left_stack, middle_stack, right_stack]
+
+#Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+while num_disks < 3:
+  num_disks = int(input("Enter a number greater than or equal to 3\n"))
+
+for i in range(num_disks, 0, -1):
+  left_stack.push(i)
+
+num_optimal_moves = (2 ** num_disks) - 1
+print("\nThe fastest you can solve this game is in {0} moves".format(num_optimal_moves))
+
+
+
+#Get User Input
+def get_input():
+  choices = [stack.get_name()[0] for stack in stacks]
+  while True:
+    for i in range(len(stacks)):
+      name = stacks[i].get_name()
+      letter = choices[i]
+
+        
+#Play the Game
